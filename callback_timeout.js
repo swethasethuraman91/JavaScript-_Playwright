b26f6ebec7b39d7dbbd8b730d0cc13c6655c4645
swethasethuraman91 <swethakeer@gmail.com>
2026-07-15 11:59:09 +0530

    Update callback_timeout.js

--- callback_timeout.js
+++ callback_timeout.js
@@ -1,13 +1,16 @@
 let browser = "Chrome";// variable declaration
+//set timeout for the function
 function checkBrowserVersion(cb)
 {
     console.log("browser will disolay after 2mins");
     setTimeout(() => {cb(browser)}, 2000);
    
 }
+//callback function
 function displayBrowser(browserName) {
   console.log("Data received from browser!" +browserName);
   
 
 }
+//function declaration
 checkBrowserVersion(displayBrowser);
