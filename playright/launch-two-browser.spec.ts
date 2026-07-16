e87e1c2e43d4132721b74532105fa58745e457e0
swethasethuraman91 <swethakeer@gmail.com>
2026-07-16 17:55:38 +0530

    Add test to launch two browser instances
    This test script launches two browser instances: one using Chromium and the other using WebKit. It navigates to specified URLs and logs the page titles and URLs.

---
+++ playright/launch-two-browser.spec.ts
@@ -0,0 +1,23 @@
+import { chromium, test, webkit } from "@playwright/test"; //import test
+  
+test("To launch a browser", async () => {
+  // 1. Launch Browser Instance A (Chromium)
+  const browserA = await chromium.launch({ headless: false, channel: "msedge" });
+  const contextA = await browserA.newContext();
+  const pageA = await contextA.newPage();
+  await pageA.goto('https://www.redbus.in');
+      console.log(await pageA.title());
+      console.log(await pageA.url());
+
+
+  // 2. Launch Browser Instance B (webkit)
+  const browserB = await webkit.launch({ headless: false , channel: "webkit" }); 
+  const contextB = await browserB.newContext();
+  const pageB = await contextB.newPage();
+  await pageB.goto('https://www.flipkart.com');
+      console.log(await pageB.title());
+      console.log(await pageB.url());
+
+  // 3. Pause script so both windows stay open on screen
+  await pageA.pause(); 
+});
